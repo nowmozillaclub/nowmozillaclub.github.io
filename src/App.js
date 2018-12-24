97b582987d36e6f4d6e028a3d902beed02986bba
Mihir Desai <zvuve@outlook.com>
2018-12-24 19:01:49 +0530

    Init CRA

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <section className="hero is-fullheight is-dark">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title is-1">Network for the Open Web Mozilla Campus Club</h1>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
