3000e6b95c8a16c69e457890bd6ff7eecf66b36d
Mihir Desai <mihir3k@gmail.com>
2018-12-26 14:32:39 +0530

    Add font and fix footer navigation padding

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
       <div className="App">
         <Helmet>
           <body className="has-navbar-fixed-top"/>
+          <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet"/>
           <script defer src="https://use.fontawesome.com/releases/v5.6.1/js/all.js" integrity="sha384-R5JkiUweZpJjELPWqttAYmYM1P3SNEJRM6ecTQF05pFFtxmCO+Y1CiUhvuDzgSVZ" crossorigin="anonymous"/>
         </Helmet>
         <Navbar/>
