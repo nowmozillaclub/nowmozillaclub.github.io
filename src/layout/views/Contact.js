22e6bbe088c983a32695cf19b7f34a218fbc5acd
Mihir Desai <mihir3k@gmail.com>
2018-12-27 18:11:45 +0530

    Add contact page content

--- src/layout/views/Contact.js
+++ src/layout/views/Contact.js
@@ -11,7 +11,39 @@ class Contact extends Component {
         <section className="hero is-fullheight-with-navbar">
           <div className="hero-body">
             <div className="container">
-              <h1 className="title has-text-centered">Contact</h1>
+              <h1 className="title is-1 has-text-centered">Say Hello! <i class="far fa-hand-spock"/></h1>
+              <div className="columns">
+                <div className="column is-3"></div>
+                <div className="column is-6">
+                  <form className="control">
+                    <div className="field">
+                      <label className="label is-medium">Name</label>
+                      <div className="control">
+                        <input className="input is-large" type="text" placeholder="Your name" required/>
+                      </div>
+                    </div>
+                    <div className="field">
+                      <label className="label is-medium">Email</label>
+                      <div className="control">
+                        <input className="input is-large" type="email" placeholder="Your email" required/>
+                      </div>
+                    </div>
+                    <div className="field">
+                      <label className="label is-medium">Message</label>
+                      <div className="control">
+                        <textarea className="textarea is-large" placeholder="Your message" required/>
+                      </div>
+                    </div>
+                    <em className="is-size-5 has-text-danger"><sup>This form has been temporarily disabled.</sup></em>
+                    <div className="field">
+                      <div className="control">
+                        <button className="button is-info is-large" disabled>Send</button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+                <div className="column is-3"></div>
+              </div>
             </div>
           </div>
         </section>
