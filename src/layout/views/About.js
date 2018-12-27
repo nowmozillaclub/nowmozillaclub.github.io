083be57e42db1b5488d6eb0ae494afea2c19fb63
Mihir Desai <zvuve@outlook.com>
2018-12-27 17:14:33 +0530

    Add about page content

--- src/layout/views/About.js
+++ src/layout/views/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 class About extends Component {
   componentDidMount() {
@@ -8,10 +9,33 @@ class About extends Component {
   render() {
     return (
       <div className="About">
-        <section className="hero is-fullheight-with-navbar">
+        <section className="hero is-fullheight-with-navbar is-warning">
           <div className="hero-body">
             <div className="container">
-              <h1 className="title has-text-centered">About</h1>
+              <h1 className="title is-1 is-spaced">About Us</h1>
+              <p className="subtitle is-3">
+                We are a <b>Mozilla Campus Club</b> at <b>NMIMS MPSTME</b>.
+              </p>
+              <p className="subtitle is-3">
+                Network for the Open Web was founded to serve as a platform for <b>Internet Education</b>.
+                We strive to <b>Learn</b>, <b>Build</b> and <b>Advocate</b> for the <b>Open Web</b>.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className="hero is-fullheight-with-navbar is-primary">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title is-1 is-spaced">Activities</h1>
+              <p className="subtitle is-3">
+                <b>Develop</b> new skills and contribute to projects.
+              </p>
+              <p className="subtitle is-3">
+                Be a part of a powerful <b>network</b> of students dedicated to keeping the internet a global public resource, <b>open</b> and <b>accessible</b> to all.
+              </p>
+              <p className="subtitle is-3">
+                <NavLink to="/events" className="button is-danger is-large">View Events</NavLink>
+              </p>
             </div>
           </div>
         </section>
