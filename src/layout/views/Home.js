1a4e3c03727b28aef6d0c2e327a43b6911bf9645
Mihir Desai <zvuve@outlook.com>
2018-12-28 19:40:25 +0530

    Add home page content

--- src/layout/views/Home.js
+++ src/layout/views/Home.js
@@ -8,10 +8,11 @@ class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <section className="hero is-fullheight-with-navbar">
+        <section className="hero is-fullheight-with-navbar is-light">
           <div className="hero-body">
             <div className="container">
-              <h1 className="title has-text-centered">Home</h1>
+              <h1 className="title is-1 is-spaced">Network for the Open Web</h1>
+              <h1 className="subtitle is-3">A Mozilla Campus Club at NMIMS MPSTME</h1>
             </div>
           </div>
         </section>
