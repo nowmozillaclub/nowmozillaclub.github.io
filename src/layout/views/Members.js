74c380ca3931e578412092de83f57532743c35a3
Mihir Desai <zvuve@outlook.com>
2018-12-28 19:39:38 +0530

    Rename Team to Members

--- src/layout/views/Members.js
+++ src/layout/views/Members.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 
-class Team extends Component {
+class Members extends Component {
   componentDidMount() {
     window.scrollTo(0, 0);
   }
   
   render() {
     return (
-      <div className="Team">
+      <div className="Members">
         <section className="hero is-fullheight-with-navbar">
           <div className="hero-body">
             <div className="container">
-              <h1 className="title has-text-centered">Team</h1>
+              <h1 className="title has-text-centered">Members</h1>
             </div>
           </div>
         </section>
@@ -20,4 +20,4 @@ class Team extends Component {
   }
 }
 
-export default Team;
+export default Members;
