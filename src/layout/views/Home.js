3ad79ace121836dbd73228998341749b855dc390
Mihir Desai <mihir3k@gmail.com>
2018-12-30 21:45:02 +0530

    Remove react-reveal fade

--- src/layout/views/Home.js
+++ src/layout/views/Home.js
@@ -9,16 +9,14 @@ class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <Fade>
-          <section className="hero is-fullheight-with-navbar is-light">
-            <div className="hero-body">
-              <div className="container">
-                <h1 className="title is-1 is-spaced">Network for the Open Web</h1>
-                <h1 className="subtitle is-3">A Mozilla Campus Club at NMIMS MPSTME</h1>
-              </div>
+        <section className="hero is-fullheight-with-navbar is-light">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title is-1 is-spaced">Network for the Open Web</h1>
+              <h1 className="subtitle is-3">A Mozilla Campus Club at NMIMS MPSTME</h1>
             </div>
-          </section>
-        </Fade>
+          </div>
+        </section>
       </div>
     );
   }
--- src/layout/views/Privacy.js
+++ src/layout/views/Privacy.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Fade from 'react-reveal/Fade';
 
 class Privacy extends Component {
   componentWillMount() {
@@ -9,23 +8,21 @@ class Privacy extends Component {
   render() {
     return (
       <div className="Privacy">
-        <Fade>
-          <section className="hero is-fullheight-with-navbar is-light">
-            <div className="hero-body">
-              <div className="container">
-                <h1 className="title is-spaced">Privacy Policy</h1>
-                <p className="subtitle">Your privacy is important to us. It is Network for the Open Web's policy to respect your privacy regarding any information we may collect from you across our website, <a className="has-text-info" href="http://nowmozillaclub.github.io" rel>http://nowmozillaclub.github.io</a>, and other sites we own and operate.</p>
-                <p className="subtitle">We only ask for personal information when we truly need it to provide a service to you. We collect it by fair and lawful means, with your knowledge and consent. We also let you know why we’re collecting it and how it will be used.</p>
-                <p className="subtitle">We only retain collected information for as long as necessary to provide you with your requested service. What data we store, we’ll protect within commercially acceptable means to prevent loss and theft, as well as unauthorised access, disclosure, copying, use or modification.</p>
-                <p className="subtitle">We don’t share any personally identifying information publicly or with third-parties, except when required to by law.</p>
-                <p className="subtitle">Our website may link to external sites that are not operated by us. Please be aware that we have no control over the content and practices of these sites, and cannot accept responsibility or liability for their respective privacy policies.</p>
-                <p className="subtitle">You are free to refuse our request for your personal information, with the understanding that we may be unable to provide you with some of your desired services.</p>
-                <p className="subtitle">Your continued use of our website will be regarded as acceptance of our practices around privacy and personal information. If you have any questions about how we handle user data and personal information, feel free to contact us.</p>
-                <p className="subtitle">This policy is effective as of 1 January 2019.</p>
-              </div>
+        <section className="hero is-fullheight-with-navbar is-light">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title is-spaced">Privacy Policy</h1>
+              <p className="subtitle">Your privacy is important to us. It is Network for the Open Web's policy to respect your privacy regarding any information we may collect from you across our website, <a className="has-text-info" href="http://nowmozillaclub.github.io" rel>http://nowmozillaclub.github.io</a>, and other sites we own and operate.</p>
+              <p className="subtitle">We only ask for personal information when we truly need it to provide a service to you. We collect it by fair and lawful means, with your knowledge and consent. We also let you know why we’re collecting it and how it will be used.</p>
+              <p className="subtitle">We only retain collected information for as long as necessary to provide you with your requested service. What data we store, we’ll protect within commercially acceptable means to prevent loss and theft, as well as unauthorised access, disclosure, copying, use or modification.</p>
+              <p className="subtitle">We don’t share any personally identifying information publicly or with third-parties, except when required to by law.</p>
+              <p className="subtitle">Our website may link to external sites that are not operated by us. Please be aware that we have no control over the content and practices of these sites, and cannot accept responsibility or liability for their respective privacy policies.</p>
+              <p className="subtitle">You are free to refuse our request for your personal information, with the understanding that we may be unable to provide you with some of your desired services.</p>
+              <p className="subtitle">Your continued use of our website will be regarded as acceptance of our practices around privacy and personal information. If you have any questions about how we handle user data and personal information, feel free to contact us.</p>
+              <p className="subtitle">This policy is effective as of 1 January 2019.</p>
             </div>
-          </section>
-        </Fade>
+          </div>
+        </section>
       </div>
     );
   }
