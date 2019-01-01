7168f1fff32cf32e09be6dd7c70574b2654b7c59
Mihir Desai <mihir3k@gmail.com>
2019-01-01 05:59:06 +0530

    Add android page changes

--- src/layout/views/Android.js
+++ src/layout/views/Android.js
@@ -14,32 +14,30 @@ class Android extends Component {
       {
         original: 'images/events/android/201801.jpg'
       }, {
-        original: 'images/events/android/201802.jpg'        
+        original: 'images/events/android/201802.jpg'
       }, {
-        original: 'images/events/android/201803.jpg'        
+        original: 'images/events/android/201803.jpg'
       }, {
-        original: 'images/events/android/201804.jpg'        
+        original: 'images/events/android/201804.jpg'
       }
     ];
     return (
       <div className="Android">
-        <section className="hero is-fullheight-with-navbar is-success">
+        <section className="hero is-fullheight-with-navbar" style={{ backgroundColor: 'mediumseagreen' }}>
           <div className="hero-body">
             <div className="container">
-              <h1 className="title is-1 is-spaced">Android Workshop</h1>
-              <p className="subtitle">
-                <b>Android App Contest</b> organised in collaboration with the <b>Computer Engineering Department</b> of <b>NMIMS MPSTME</b>.
+              <h1 className="title is-1 is-spaced" style={{ color: 'white' }}>Android Workshop</h1>
+              <p className="subtitle is-3" style={{ color: 'white' }}>
+                Participate in the annual <b>Android App Contest</b> and stand a chance to <b>win exciting prizes!</b>
+              </p>
+              <p className="subtitle" style={{ color: 'white' }}>
+                Organised in collaboration with Computer Engineering Department, NMIMS MPSTME.
               </p>
               <p className="subtitle is-3">
                 <NavLink to="/register" className="button is-dark is-large">Register Now</NavLink>
               </p>
-            </div>
-          </div>
-        </section>
-        <section className="hero is-fullheight-with-navbar is-success">
-          <div className="hero-body">
-            <div className="container">
-              <p className="title is-3">2018 Photos</p>
+              <br/><br/>
+              <p className="title is-3" style={{ color: 'white' }}>2018 Photos</p>
               <div style={{ border: '2px solid white', borderRadius: '2px' }}>
                 <ImageGallery items={images} showPlayButton={false} showThumbnails={false}/>
               </div>
@@ -50,36 +48,29 @@ class Android extends Component {
                   <span className="icon"><i className="fab fa-facebook-f"/></span>
                   <span>View More</span>
               </a>
-            </div>
-          </div>
-        </section>
-        <section className="hero is-fullheight-with-navbar is-success">
-          <div className="hero-body">
-            <div className="container">
-              <p className="title is-3">Rules &amp; Regulations</p>
-              <p className="title is-4">
+              <br/><br/><br/>
+              <p className="title is-3" style={{ color: 'white' }}>Rules &amp; Regulations</p>
+              <p className="title is-4" style={{ color: 'white' }}>
                 1. Who Should Apply?
               </p>
-              <p className="subtitle is-5" style={{ lineHeight: '1.6' }}>
+              <p className="subtitle is-5" style={{ color: 'white', lineHeight: '1.6' }}>
                 Students are invited to compete individually or in teams of up to four student members.
                 Any student currently in their 2nd year of an undergraduate course at NMIMS MPSTME can compete.
                 Teams must adhere to the deadline timetable as defined by the Computer Engineering Department.
                 Teams must not accept or use any help from people not in their team.
                 Usage of free and/or open source Internet resources is allowed, but plagiarism and cheating is not.
-              </p>
-              <br/>
-              <p className="title is-4">
+              </p>              
+              <p className="title is-4" style={{ color: 'white' }}>
                 2. How To Apply?
               </p>
-              <p className="subtitle is-5" style={{ lineHeight: '1.6' }}>
+              <p className="subtitle is-5" style={{ color: 'white', lineHeight: '1.6' }}>
                 A team of upto four students are invited to devise and develop mobile applications that be will be beneficial to science, technology, education or health.
                 Register your team by entering your details in the form below.
-              </p>
-              <br/>
-              <p className="title is-4">
+              </p>              
+              <p className="title is-4" style={{ color: 'white' }}>
                 Win Amazing Prizes
               </p>
-              <p className="subtitle is-5" style={{ lineHeight: '1.6' }}>
+              <p className="subtitle is-5" style={{ color: 'white', lineHeight: '1.6' }}>
                 We have awesome prizes for you plus all participants who submit an app will be given a certificate of participation.
               </p>
               <br/>
