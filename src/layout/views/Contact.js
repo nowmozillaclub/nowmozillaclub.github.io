f7ef81dc3ee61ec83f0c863875cc6c7b87084ffb
Mihir Desai <zvuve@outlook.com>
2019-01-02 18:16:34 +0530

    Update contact page

--- src/layout/views/Contact.js
+++ src/layout/views/Contact.js
@@ -8,42 +8,43 @@ class Contact extends Component {
   render() {
     return (
       <div className="Contact">
-        <section className="hero is-fullheight-with-navbar">
+        <section className="hero is-medium is-light">
           <div className="hero-body">
             <div className="container">
-              <h1 className="title is-1 has-text-centered">Say Hello! <i class="far fa-hand-spock"/></h1>
-              <div className="columns">
-                <div className="column is-3"></div>
-                <div className="column is-6">
-                  <form className="control">
-                    <div className="field">
-                      <label className="label is-medium">Name</label>
-                      <div className="control">
-                        <input className="input is-large" type="text" placeholder="Your name" required/>
-                      </div>
-                    </div>
-                    <div className="field">
-                      <label className="label is-medium">Email</label>
-                      <div className="control">
-                        <input className="input is-large" type="email" placeholder="Your email" required/>
-                      </div>
-                    </div>
-                    <div className="field">
-                      <label className="label is-medium">Message</label>
-                      <div className="control">
-                        <textarea className="textarea is-large" placeholder="Your message" required/>
-                      </div>
-                    </div>
-                    <em className="is-size-5 has-text-danger"><sup>This form has been temporarily disabled.</sup></em>
-                    <div className="field">
-                      <div className="control">
-                        <button className="button is-info is-large" disabled>Send</button>
-                      </div>
-                    </div>
-                  </form>
-                </div>
-                <div className="column is-3"></div>
+              <h1 className="title is-1">Say Hello! <i class="far fa-hand-spock"/></h1>
+              <div className="buttons">
+                <a className="button is-large is-dark" 
+                  href="mailto:nowmozillaclub@gmail.com"
+                  style={{ backgroundColor: '#ffb94a' }}
+                  target="_blank" rel="noopener noreferrer">
+                    <span className="icon is-small"><i className="fas fa-envelope fa-lg"/></span>
+                </a>
+                <a className="button is-large is-dark" 
+                  href="https://facebook.com/nowmozillaclub"
+                  style={{ backgroundColor: '#3b5998' }}
+                  target="_blank" rel="noopener noreferrer">
+                    <span className="icon is-small"><i className="fab fa-facebook-f fa-lg"/></span>
+                </a>
+                <a className="button is-large is-dark" 
+                  href="https://instagram.com/nowmozillaclub"
+                  style={{ backgroundColor: '#c13584' }}
+                  target="_blank" rel="noopener noreferrer">
+                    <span className="icon is-small"><i className="fab fa-instagram fa-lg"/></span>
+                </a>
+                <a className="button is-large is-dark" 
+                  href="https://twitter.com/nowmozillaclub"
+                  style={{ backgroundColor: '#1da1f2' }}
+                  target="_blank" rel="noopener noreferrer">
+                    <span className="icon is-small"><i className="fab fa-twitter fa-lg"/></span>
+                </a>
+                <a className="button is-large is-dark" 
+                  href="https://github.com/nowmozillaclub"
+                  style={{ backgroundColor: '#333333' }}
+                  target="_blank" rel="noopener noreferrer">
+                    <span className="icon is-small"><i className="fab fa-github fa-lg"/></span>
+                </a>
               </div>
+              <p className="subtitle is-5">nowmozillaclub@gmail.com</p>
             </div>
           </div>
         </section>
